3f08b0d1178965b26ac1a95abb0255d517f1a2df
botagari <48246327+botagari@users.noreply.github.com>
2019-05-11 01:17:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -270,39 +270,6 @@ client.on('guildCreate', guild => {
 })
 
 
-var Enmap = require('enmap');
-client.antibots = new Enmap({name: "antibot"});
-var antibots = client.antibots;
-var julian = client;
-julian.on("message", codes => {
-var prefix = "-";
-if(codes.content.startsWith("$antibots on")){
-if(codes.author.bot || !codes.channel.guild || codes.author.id != codes.guild.ownerID) return;
-antibots.set(`${codes.guild.id}`, {
-onoff: 'On'
-});
- 
- 
-codes.channel.send("AntiBots Join Is On");
-}
-if(codes.content.startsWith("$antibots off")){
-if(codes.author.bot || !codes.channel.guild || codes.author.id != codes.guild.ownerID) return;
-antibots.set(`${codes.guild.id}`, {
-onoff: "Off"
-});
-codes.channel.send("AntiBots Join Is Off");
-}
-});
- 
-julian.on("guildMemberAdd", member => {
-if(!antibots.get(`${member.guild.id}`)) { antibots.set(`${member.guild.id}`, {
-onoff: "Off"
-});
-}
-if(antibots.get(`${member.guild.id}`).onoff == "Off") return;
-if(member.user.bot) return member.kick()
-});
-
 
 client.on("message", (message) => {
  
