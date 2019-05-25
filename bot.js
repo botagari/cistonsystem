be7eb3556daf267cd7274f2eb2d9e78ce46152ac
botagari <48246327+botagari@users.noreply.github.com>
2019-05-25 14:02:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['567694723679125514	'];
+const devs = ['567694723679125514'];
 const db = require('quick.db');
 const premium = ['567694723679125514']
 const client = new Discord.Client();   
